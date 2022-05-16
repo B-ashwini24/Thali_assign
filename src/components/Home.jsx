701619785f31a3e0711a22d6dbc31a2807d57587
B-ashwini24 <ash24biradar@gmail.com>
2022-05-16 17:06:10 +0530

    thali

---
+++ src/components/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import img1 from '../images/thali.jpg'
+const Home = () => {
+  return (
+    <div style={{ backgroundImage: 'url('+img1+')',height: "100vh",
+    }}>
+      <h1>Vihaa's Restro</h1>
+
+      
+    </div>
+  )
+}
+
+export default Home
---
+++ src/components/Show.jsx
@@ -0,0 +1,82 @@
+import React, { useState } from 'react'
+import { Card } from 'antd';
+import { Row, Col } from 'antd';
+import { Button } from 'antd';
+import productslice, { deletefromcart } from '../slices/productslice';
+import { useDispatch, useSelector } from 'react-redux'
+import { Navigate, useNavigate } from 'react-router-dom';
+
+const { Meta } = Card;
+
+const Show = () => {
+  const data=useSelector(state=>state.productslice.cart)
+  const price=useSelector(state=>state.productslice.totalPrice)
+  const [flag,setFlag]=useState('small')
+  //console.log(data)
+  const navigate=useNavigate();
+  const dispatch=useDispatch();
+  //const data=carts.cart;
+const removefromcart=(ele)=>{
+console.log(ele)
+  dispatch(deletefromcart(ele.id))
+}
+const move=()=>{
+  navigate("/Paypage")
+}
+  const chekout=()=>{
+    if(data.length<2)
+    {
+      alert("Cart must have atleast 2 items")
+     
+    }
+    else{
+      move()
+    }
+  }
+
+  return (
+    <div >
+       <div style={{paddingLeft:'10%',paddingTop:'0px'}}>
+        <h1  style={{paddingLeft:'40%',paddingTop:'0px',color:'red'}}>Your Cart</h1>
+        <Button type="primary" onClick={chekout}>Checkout</Button></div>
+      <h1 style={{paddingLeft:'10%',color:'red'}}>Your Bill:{price}</h1>
+   
+   
+     
+   
+    
+ 
+<Row>
+        {
+         
+            data.map((ele,id)=>(
+                <Col >
+                    <div >
+                    <Card 
+                        hoverable
+                        style={{height:'400px',width:'400px' ,display:'flex' }}
+                        cover={<img alt="example" src={ele.imgdata} />}
+                      >
+                        <Meta title={ele.name}  description={ele.price} /><br/>
+                        <Button type="primary" onClick={()=>removefromcart(ele)}>Remove from cart</Button>
+                      </Card>
+                      
+                      </div>
+                  </Col>
+            ))
+            
+          
+            
+        }
+        
+     
+    </Row>  
+  
+
+    </div>
+   
+    
+  )
+}
+
+export default Show
